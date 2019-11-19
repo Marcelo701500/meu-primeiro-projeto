2792b49b32e79a7781b0f52d5b7f45e0c044a99c
Marcelo Oliveira <marcelo.oliveira@juspro.com.br>
2019-11-19 12:32:56 -0300

    Versão inicial do projeto

---
+++ src/test/java/com/oliveira/api/MeuPrimeiroProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oliveira.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPrimeiroProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
